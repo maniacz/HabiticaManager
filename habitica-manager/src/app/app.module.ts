ae075a42ee99e4934a300fedee1ed59634d8c7a4
Łukasz Kipka <lkipka@gmail.com>
2024-05-28 09:17:29 +0200

    Add loading spinner

--- habitica-manager/src/app/app.module.ts
+++ habitica-manager/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodosListComponent } from './components/todos-list/todos-list.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { TagFiltersComponent } from './components/tag-filters/tag-filters.component';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodosListComponent,
-    TagFiltersComponent
+    TagFiltersComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,9 @@ import { TagFiltersComponent } from './components/tag-filters/tag-filters.compon
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ habitica-manager/src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { LoadingService } from '../../services/loading.service';
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: `
+    <div class="loading-overlay" *ngIf="loading$ | async">
+      <div class="spinner"></div>
+    </div>
+  `,
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent {
+  loading$ = this.loadingService.loading$;
+
+  constructor(private loadingService: LoadingService) {}
+}
