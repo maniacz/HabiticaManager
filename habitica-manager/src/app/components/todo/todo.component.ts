f4c648a9e3d316ffea578eb252779d114ab8696e
Łukasz Kipka <lkipka@gmail.com>
2025-02-03 15:26:46 +0100

    Display emojis

--- habitica-manager/src/app/components/todo/todo.component.ts
+++ habitica-manager/src/app/components/todo/todo.component.ts
@@ -3,6 +3,7 @@ import { Tag } from '../../models/tag';
 import { TodoEmojiValidatorService } from '../../services/todo-emoji-validator.service';
 import { TodoValidationResult } from '../../models/todo-validation-result';
 import { TodoValidation } from '../../enums/todo-validation';
+import emoji from 'emoji-dictionary';
 
 @Component({
   selector: 'app-todo',
@@ -16,12 +17,31 @@ export class TodoComponent implements OnInit {
   @Input() isSelected: boolean = false;
   @Output() isSelectedChange = new EventEmitter<boolean>();
   validationResult!: TodoValidationResult;
+  emojiText = String.fromCodePoint(0x1F602) + " " + String.fromCodePoint(0x1F4BB) + " " + String.fromCodePoint(0x1F60A);
 
   constructor(private todoEmojiValidator: TodoEmojiValidatorService) {}
 
   ngOnInit(): void {
     this.validationResult = this.todoEmojiValidator.validate(this.taskName, this.assignedTags);
+    this.convertTextToEmoji(this.taskName);
   }
+
+  convertTextToEmoji(text: string) {
+    // Find all emoji text surrounded by colons in the task name
+    const regEx = /:([^\s:]+):/g;
+    const matches = text.match(regEx)
+    matches?.forEach((match) => {
+      // Strip surrounding colons
+      const emojiText = match.slice(1, -1);
+      console.log(emojiText);
+      const unicode = emoji.getUnicode(emojiText);
+      console.log(unicode);
+      // Replace emoji text with emoji
+      text = text.replace(match, unicode);
+      console.log(text);
+      this.taskName = text;
+    });
+}
 
   setBackgroundColor() {
     switch (this.validationResult.result) {
